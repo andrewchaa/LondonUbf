98b8604d13472bae6885ea09c1b894098f928598
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2012-06-10 22:32:14 +0100

    revising content parser

--- LondonUbf.Domain/FileContentParser.cs
+++ LondonUbf.Domain/FileContentParser.cs
@@ -7,25 +7,35 @@ namespace LondonUbf.Domain
     public class FileContentParser : IMessageParser
     {
         private readonly static Regex YearBookLectureNopattern = new Regex(@"^(?<Year>[0-9]+?)\s(?<Book>[0-9A-Za-z]+?)\s(?<LectureNo>[0-9]+?)\s");
+        private readonly static Regex YearPattern = new Regex(@"^(?<Year>[0-9]+?)\s");
         private readonly static Regex Chapterpattern = new Regex(@"\s(?<Chapter>[0-9:-]+?)$");
 
         public ServiceMessage Parse(string content)
         {
-            var lines = content.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            string cleanedUpContent = content.Replace("\\t", string.Empty);
 
-            if (!YearBookLectureNopattern.IsMatch(lines[0]))
-                return new ServiceMessage();
+            var lines = cleanedUpContent.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
 
             var message = new ServiceMessage();
-            var bookLectureNoMatch = YearBookLectureNopattern.Match(lines[0]);
-            message.Year = int.Parse(bookLectureNoMatch.Groups["Year"].Value);
-            message.Book = bookLectureNoMatch.Groups["Book"].Value;
-            message.LectureNo = int.Parse(bookLectureNoMatch.Groups["LectureNo"].Value);
 
-            message.Title = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(lines[1].ToLower());
+            if (YearPattern.IsMatch(lines[0]))
+            {
+                message.Year = int.Parse(YearPattern.Match(lines[0]).Groups["Year"].Value);
+            }
 
-            var chapterMatch = Chapterpattern.Match(lines[2]);
-            message.Chapter = chapterMatch.Groups["Chapter"].Value;
+//
+//            if (!YearBookLectureNopattern.IsMatch(lines[0]))
+//                return new ServiceMessage();
+//
+//            var bookLectureNoMatch = YearBookLectureNopattern.Match(lines[0]);
+//            message.Year = int.Parse(bookLectureNoMatch.Groups["Year"].Value);
+//            message.Book = bookLectureNoMatch.Groups["Book"].Value;
+//            message.LectureNo = int.Parse(bookLectureNoMatch.Groups["LectureNo"].Value);
+//
+//            message.Title = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(lines[1].ToLower());
+//
+//            var chapterMatch = Chapterpattern.Match(lines[2]);
+//            message.Chapter = chapterMatch.Groups["Chapter"].Value;
 
             return message;
         }
--- LondonUbf.Test/Unit/FileContentParsingTest.cs
+++ LondonUbf.Test/Unit/FileContentParsingTest.cs
@@ -15,6 +15,17 @@ namespace LondonUbf.Test.Unit
         }
 
         [Test]
+        public void Should_Parse_Year()
+        {
+            const string content =
+                @"2011 Luke Study                                              \t\t\t\t\t      22 May 2011";
+
+            var message = _parser.Parse(content);
+
+            Assert.That(message.Year, Is.EqualTo(2011));
+        }
+
+        [Test]
         public void Should_Parse_The_Content()
         {
             const string messageFileContent =
@@ -60,5 +71,31 @@ Prayer is communication with God. In this passage Jesus teaches us how to commun
             Assert.That(message.Book, Is.Null);
 
         }
+
+        [Test]
+        public void Should_Remove_Tab_Characters()
+        {
+            const string content = @"2011 Luke Study                                              \t\t\t\t\t      22 May 2011
+
+YOUR KINGDOM COME
+
+Luke 11:1-13
+Key verse 11:2
+‘He said to them, When you pray, say: 'Father, hallowed be your name, your kingdom come.'’
+
+
+Prayer is communication with God. In this passage Jesus teaches us how to communicate with God. In particular he teaches us two things; what to include in our prayers and the correct attitude in prayer. God wants a father / child relationship with us and that he wants us to pray boldly for his glory, that his kingdom would come and for our needs. And when we bring all of these things together we learn that our prayer lives should be dynamic, powerful and refreshing. We should have prayer lives which reflect what God wants and have confidence that he will answer. I pray that through today’s message we refresh our prayer lives and build a deeper relationship with our father God.
+";
+
+            var message = _parser.Parse(content);
+
+            Assert.That(message.Year, Is.EqualTo(2011));
+            Assert.That(message.Book, Is.EqualTo("Luke"));
+            Assert.That(message.LectureNo, Is.EqualTo(20));
+            Assert.That(message.Title, Is.EqualTo("Your Kingdom Come"));
+            Assert.That(message.Chapter, Is.EqualTo("11:1-13"));
+        }
+        
+
     }
 }
