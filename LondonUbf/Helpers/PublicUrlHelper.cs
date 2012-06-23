ce5225fa3c941eda03edcc724311042a8942f9ea
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2012-06-23 19:17:32 +0100

    fix absolute url with port number

---
+++ LondonUbf/Helpers/PublicUrlHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LondonUbf.Helpers
+{
+//    public static class PublicUrlHelper
+//    {
+//        public static string ToPublicUrl(this UrlHelper urlHelper, Uri relativeUri)
+//        {
+//            var httpContext = urlHelper.RequestContext.HttpContext;
+//
+//            var uriBuilder
+//        }
+//    }
+}
